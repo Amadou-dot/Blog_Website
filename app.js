a0b7873bd4f143bf7ebc06f56eed823ce64c30ac
Amadou <amadou.sseck@gmail.com>
2022-05-05 12:07:28 -0600

    feat: posts with over 100 chars now lead to a seperate page of their own for viewing. Posts with less than 100 chars no longer have a read more button

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const date = require(path.join(__dirname, 'date.js'))
+const postsData = require(path.join(__dirname, 'posts.json'))
+const fs = require('fs')
 const port = 3000
-const posts = []
+let postCounter = 0
+let postNumber;
 
 // app config
 app.set('view engine', 'ejs')
@@ -14,23 +17,41 @@ app.use(express.urlencoded({ extended: true }))
 // web directories
 app.get('/', (_req, res) => res.redirect('/home'))
 app.get('/home', (_req, res) => {
-    res.render('home', {posts})
+    res.render('home', {posts:postsData.posts})
 })
+
 app.post('/home', (req, res) => {
     const title = req.body.title
     const body = req.body.post_body
     let first_100_chars = body.substring(0,100)
-    posts.push({
+    const newContent = {
+        post_number: postCounter,
         date_created: date.getDateAndTime(),
         title,
         body,
-        first_100_chars
+        first_100_chars,
+    }
+    postCounter++
+    postsData.posts.push(newContent)
+    // write the new content to the json file so we can save it and load it later
+    fs.writeFile(path.join(__dirname, 'posts.json'), JSON.stringify(postsData), (err) => {
+        if (err) console.error(err)
     })
-    
     res.redirect('/home')
     
 })
 
+app.post('/post', (req, res) => {
+    postNumber = req.body.post_number
+    res.render('post',{post:postsData.posts[postNumber]})
+})
+app.get(`/clear`,(req, res) => {
+    postsData.posts = []
+    fs.writeFile(path.join(__dirname, 'posts.json'), JSON.stringify(postsData), (err) => {
+        console.error(err)
+    })
+    res.redirect('/home')
+})
 app.get('/about', (_req, res) => res.render('about'))
 app.get('/contact', (_req, res) => res.render('contact'))
 app.get('/compose', (_req, res) => res.render('compose'))
